c22d4f1f9648b7ef1981cc2d31804a7882f46ba5
dhritia <81909460+dhritia@users.noreply.github.com>
2023-12-04 14:03:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// Check connection status
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB at localhost');
+});
 import UserRoutes from "./users/routes.js";
 const app = express();
 app.use(express.json());
